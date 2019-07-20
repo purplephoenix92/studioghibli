a48d690065263fcb57b8be9bd2506576935461a7
Angela Edwards <edwards11914@outlook.com>
2019-07-20 15:11:26 -0400

    working now

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,37 +1,86 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
 
-    return (
+class Navbar extends React.Component {
 
-        <nav className="navbar navbar-inverse">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    {/* <NavBrand linkTo={this.props.brand.linkTo} text={this.props.brand.text} /> */}
-                </div>
-                <div className="collapse navbar-collapse" id="navbar-collapse">
+    constructor() {
 
-                    <ul className="nav navbar-nav"> <Link to="/" className="active">Home</Link>
-                        <li><Link to="/Locations">Locations</Link></li>
-                        <li><Link to="/Characters">Characters</Link></li>
-                        <li><Link to="/Films">Films</Link></li>
+        super();
+        this.state = {
+            active: false 
+        }
+    }
+
+    toggleClass = () => {
+        const currentState = this.state.active;
+        this.setState({ active: !currentState });
+        console.log('toggle')
+    };
+
+
+    render() {
+        return (
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" onClick={this.toggleClass} data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        {/* This is the part of the code that enables the hamburger menu. Icon-bar is just a horizontal bar that is seen when the viewer looks at the website in mobile view */}
+                    </div>
+                    <div className={this.state.active ? 'navbar-collapse': 'navbar-collapse collapse'}  id="navbar-collapse">
+
+                        <ul className="nav navbar-nav"> <Link to="/" className="active">Home</Link>
+                            <li><Link to="/Locations">Locations</Link></li>
+                            <li><Link to="/Characters">Characters</Link></li>
+                            <li><Link to="/Films">Films</Link></li>
                     </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-
-    )
-
-
-
-
+            </nav>
+        )
+    }
 }
+
+
+
+
+
+// const Navbar = () => {
+
+//     // return (
+
+//     //     <nav className="navbar navbar-inverse">
+//     //         <div className="container-fluid">
+//     //             <div className="navbar-header">
+//     //                 <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+//     //                     <span className="sr-only">Toggle navigation</span>
+//     //                     <span className="icon-bar"></span>
+//     //                     <span className="icon-bar"></span>
+//     //                     <span className="icon-bar"></span>
+//     //                 </button>
+//     //                 {/* This is the part of the code that enables the hamburger menu. Icon-bar is just a horizontal bar that is seen when the viewer looks at the website in mobile view */}
+//     //             </div>
+//     //             <div className="collapse navbar-collapse" id="navbar-collapse">
+
+//     //                 <ul className="nav navbar-nav"> <Link to="/" className="active">Home</Link>
+//     //                     <li><Link to="/Locations">Locations</Link></li>
+//     //                     <li><Link to="/Characters">Characters</Link></li>
+//     //                     <li><Link to="/Films">Films</Link></li>
+//     //                 </ul>
+//     //             </div>
+//     //         </div>
+//     //     </nav>
+// // 
+//     )
+
+
+
+
+// }
 
 export default Navbar 
